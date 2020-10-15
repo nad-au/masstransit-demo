afc61ac8e8cbb24c729e9d14912452adbd13be21
Neil Dobson <neildobson@propertyme.com>
2020-10-15 22:07:30 +1100

    Add sample

---
+++ MassTransitDemo/MassTransitDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MassTransitDemo
+{
+    public class Message
+    { 
+        public string Text { get; set; }
+    }
+
+
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var services = new ServiceCollection();
+
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<SubmitOrderConsumer>(typeof(SubmitOrderConsumerDefinition));
+                
+                x.UsingInMemory();
+            });
+            services.AddMassTransitHostedService();
+
+            var provider = services.BuildServiceProvider();
+
+            var busControl = provider.GetRequiredService<IBusControl>();
+
+            await busControl.StartAsync(new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
+
+            await busControl.Publish(new SubmitOrder
+            {
+                OrderId = 1
+            });
+            
+            try
+            {
+                Console.WriteLine("Press enter to exit");
+
+                await Task.Run(() => Console.ReadLine());
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
